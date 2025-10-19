07d2df44bc8f2ef8a3ac47933aaf3408c62cb163
HarryYin03 <harryying03@gmail.com>
2025-10-19 16:06:21 +0100

    implement features

--- xoro/src/sections/Hero.jsx
+++ xoro/src/sections/Hero.jsx
@@ -5,7 +5,7 @@ import Button from '../components/Button.jsx';
 
 const Hero = () => {
     return (
-        <section className="relative pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 pt-60 border-2 border-amber-400" >
+        <section className="relative pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 pt-60" >
             <Element name="hero">
                 <div className="container">
                     <div className="relative z-2 max-w-512 max-lg:max-w-388">
